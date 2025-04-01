97d91bd6352e6ba6b6f27fb78309bfe1bb076fe3
Alejandro Aldea <alejandro.aldea@usach.cl>
2025-03-31 23:10:37 -0300

    Primer acercamiento a un mod

---
+++ ModCitiesSkylines/ModCitiesSkylines/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//Referencias de la biblioteca de Cities Skylines
+using ICities; // API de Cities Skylines para modding
+using ColossalFramework; // Biblioteca para Cities Skylines
+using UnityEngine; // Biblioteca de Unity para manipulacion del motor grafico y otros elementos
+
+
+namespace ModCitiesSkylines
+{
+    public class Class1 : IUserMod  // Permite que el mod sea reconocido por Cities Skylines
+    {
+        public string Name // Permite darle el nombre al mod
+        {
+            get { return "bGames-Cities:Skyline"; }
+        }
+        public string Description // Permite darle una descripción al mod
+        {
+            get { return "Un mod el cual incorpora las actividades del usuario en la experiencia de juego"; }
+        }
+    }
+}
